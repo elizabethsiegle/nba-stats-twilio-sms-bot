48f0dd9b90855b8a508aea4137a5bd95cd999b97
Lizzie Siegle <esiegle@brynmawr.edu>
2017-06-14 18:04:52 -0400

    simplified version for blog post

--- app.py
+++ app.py
@@ -1,37 +1,36 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from openpyxl import load_workbook, Workbook 
-from openpyxl.compat import range
-from openpyxl.utils import get_column_letter
 
-new_wb = Workbook()
-destination_file = "nba_stats_texted.xlsx"
-ws1 = new_wb.active
-ws1.title = "NBA stats texted"
 
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 
 def parseExcel():
+	excelfile = 'nba_stats_two_sheets.xlsx'
+	typomsg = "send first and last name(s), stat, or check for typos!"
+	statmsg = "Type 1-2 players' names(first + last, players separated by a space), then a stat(GP,W,L,MIN,PTS,FG%,3P%,FT%,REB,AST,STL,BLK)"
 	msg = request.form['Body'].lower() #check that it's lowercase
 	list_of_players = []
 	list_of_stats = []
-	#AGE	GP	W	L	MIN	PTS	FGM	FGA	FG%	3PM	3PA	3P%	FTM	FTA	FT%	OREB	DREB	REB	AST	TOV	STL	BLK	PF	DD2	TD3	
-	stat_dict = { #make dictionary: key = stat, value=column of Excel spreadsheet (column B is avg minutes played)
-	"age":"B", "gp":"C","w":"D","l":"E","min":"F","pts":"G",
-	"fgm":"H","fga":"I","fg%":"J","3pm":"K","3pa":"L","ftm":"M","fta":"N",
-	"ft%":"O","oreb":"P","dreb":"Q","reb":"R","ast":"S","tov":"T", "stl": "U",
-	"blk": "V", "pf": "W", "dd2": "X", "td3": "Y", 
+	#make dictionary: key = stat, value=column of Excel spreadsheet (column B is avg minutes played)
+	
+	stat_dict = { 
+		"age":"B", "gp":"C","w":"D","l":"E","min":"F","pts":"G",
+		"fgm":"H","fga":"I","fg%":"J",
+		"3pm":"K","3pa":"L","ftm":"M","fta":"N",
+		"ft%":"O","oreb":"P","dreb":"Q","reb":"R","ast":"S","tov":"T", "stl": "U",
+		"blk": "V", "pf": "W", "dd2": "X", "td3": "Y"
 	}
-	wb = load_workbook('nba_stats_2_sheets.xlsx')
+	wb = load_workbook(excelfile)
 	reg_season = True # set boolean, will change depending on user input
 	if msg == "play":
 		ret = MessagingResponse().message("type \'a\' for regular season or \'b\' for playoffs")
 	elif msg == 'a':
-		ret = MessagingResponse().message("Type 1-2 players' names(first + last, players separated by a space), then a stat(GP,W,L,MIN,PTS,FG%,3P%,FT%,REB,AST,STL,BLK)")
+		ret = MessagingResponse().message(statmsg)
 	elif msg == 'b':
 		reg_season = False
-		ret = MessagingResponse().message("Type 1-2 players' names(first + last name, players separated by a space), then a stat(GP,W,L,MIN,PTS,FG%,3P%,FT%,REB,AST,STL,BLK)")
+		ret = MessagingResponse().message(statmsg)
 	else:
 		if reg_season: # playoffs or regular season, take your pick!!
 			ws = wb[wb.sheetnames[0]] #first sheet
@@ -52,7 +51,7 @@ def parseExcel():
 			if full_name in player_stat_map.keys():
 				ret = MessagingResponse().message(full_name + " averaged " + str(stat) + " of: " +str(player_stat_map[full_name]))
 			else:
-				ret = MessagingResponse().message("send first and last name(s) and stat. to check, or check for typos!")
+				ret = MessagingResponse().message(typomsg)
 		elif len(player_and_stat) == 5: #2 players
 			player1 = player_and_stat[0] + " " + player_and_stat[1] 
 			player2 = player_and_stat[2] + " " + player_and_stat[3]
@@ -67,13 +66,13 @@ def parseExcel():
 			player_stat_map = dict(zip(list_of_players, list_of_stats))
 			if player1 in player_stat_map.keys() and player2 in player_stat_map.keys():
 				if player_stat_map[player1] > player_stat_map[player2]:
-					ret = MessagingResponse().message(player1 + " averaged " + str(player_stat_map[player1]) + ", higher than " + player2 + "\'s " + str(player_stat_map[player2]))
+					ret = MessagingResponse().message(player1 + " 's total regular season " +  stat + " were " + str(player_stat_map[player1]) + ", higher than " + player2 + "\'s " + str(player_stat_map[player2]))
 				else:
-					ret = MessagingResponse().message(player2 + " averaged " + str(player_stat_map[player2]) + ", higher than " + player1 + "\'s " + str(player_stat_map[player1]))
+					ret = MessagingResponse().message(player2 + " 's total regular season " + stat + " " + str(player_stat_map[player2]) + ", higher than " + player1 + "\'s " + str(player_stat_map[player1]))
 			else: #check
 				ret = MessagingResponse().message("check both players' names (first and last!)")
 		else: #idk how many players
-			ret = MessagingResponse().message("send first and last name(s), or check for typos!")
+			ret = MessagingResponse().message(typomsg)
 	return str(ret)
 
 if __name__ == "__main__":
---
+++ simplifiedapp.py
@@ -0,0 +1,54 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+from openpyxl import load_workbook, Workbook 
+
+
+def parseDataIntoDict(data):
+	list_of_players = []
+	list_of_stats = []
+	stat_dict = { 
+		"age":"B", "gp":"C","w":"D","l":"E","min":"F","pts":"G",
+		"fgm":"H","fga":"I","fg%":"J",
+		"3pm":"K","3pa":"L","ftm":"M","fta":"N",
+		"ft%":"O","oreb":"P","dreb":"Q","reb":"R","ast":"S","tov":"T", "stl": "U",
+		"blk": "V", "pf": "W", "dd2": "X", "td3": "Y"
+	}
+	excelfile = 'nba_stats_two_sheets.xlsx'
+	wb = load_workbook(excelfile)
+	ws = wb[wb.sheetnames[0]]
+	for row in range(1, ws.max_row+1): #need +1 to get last row!
+			for col in "A": #A gets players for texted season
+				cell_name="{}{}".format(col, row)
+				list_of_players.append(ws[cell_name].value.lower())
+			for col in stat_dict[data]: # gets column of whatever statistic
+				cell_name="{}{}".format(col, row)
+				#print(ws[cell_name].value)
+				list_of_stats.append(ws[cell_name].value)
+	return dict(zip(list_of_players, list_of_stats))
+
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+
+def sendSms():
+	msg = request.form['Body'].lower() # convert to lowercase
+	typomsg = "send 1st + last names of 2 players followed by a stat (GP,W,L,MIN,PTS,FG%,3P%,FT%,REB,AST,STL,BLK). Check for typos!"
+	player_and_stat = msg.split() #split 
+
+	if len(player_and_stat) == 5: # check input: 2 players + stat
+		player1 = player_and_stat[0] + " " + player_and_stat[1] 
+		player2 = player_and_stat[2] + " " + player_and_stat[3]
+		stat = player_and_stat[4]
+		player_stat_map = parseDataIntoDict(stat)
+		if player1 in player_stat_map.keys() and player2 in player_stat_map.keys():
+			if player_stat_map[player1] > player_stat_map[player2]:
+				ret = MessagingResponse().message(player1 + " 's total " + str(player_stat_map[player1]) + ", higher than " + player2 + "\'s " + str(player_stat_map[player2]))
+			else:
+				ret = MessagingResponse().message(player2 + " 's total " + str(player_stat_map[player2]) + ", higher than " + player1 + "\'s " + str(player_stat_map[player1]))
+		else: #check
+			ret = MessagingResponse().message("check both players' names (first and last!)")
+	else: #idk how many players
+		ret = MessagingResponse().message(typomsg)
+	return str(ret)
+
+if __name__ == "__main__":
+	app.run(debug=True)
